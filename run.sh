cd653e57798d539139139e87dd10ce7e2cd21d2b
Vinicius Kastrup <viniciusmkm@gmail.com>
2015-06-02 16:19:56 -0300

    add support to multiple ssl certificates

--- run.sh
+++ run.sh
@@ -16,8 +16,18 @@ if [ -n "$SSL_CERT" ]; then
     echo "SSL certificate provided!"
     echo -e "${SSL_CERT}" > /servercert.pem
     export SSL="ssl crt /servercert.pem"
+elif [ -n "$SSL_CERTS_COUNT" ]; then
+    echo "Multiple SSL certificates provided!"
+    mkdir /certs
+
+    for i in `seq 0 $SSL_CERTS_COUNT`; do
+        var="SSL_CERT_$i"
+        echo -e "${!var}" > /certs/${i}_servercert.pem
+    done
+
+    export SSL="ssl crt /certs/"
 else
     echo "No SSL certificate provided"
 fi
 
-exec python /app/haproxy.py 
+exec python /app/haproxy.py
